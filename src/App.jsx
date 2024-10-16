7b3038469ad6d7f83671fc14f9f2f795f292d434
abidoye korede <korexcoded@gmail.com>
2024-10-16 15:18:37 +0100

    updated files

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <main>
-        <About />
-        <Projects />
-        <Contact />
-      </main>
+      
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+   <div className='bg-red-800'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo, ullam odit. Dicta praesentium autem vitae enim veritatis, obcaecati laboriosam molestiae labore, cumque doloremque tempora quos eligendi unde? Similique, possimus ducimus!</div>
+
   )
 }
 
