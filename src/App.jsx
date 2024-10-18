8c26e89ad9321a42257e3622a6e93407de2a72d1
abidoye korede <korexcoded@gmail.com>
2024-10-18 14:00:53 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className='bg-black-600 '>
+    <div className=' '>
       <Navbar />
       
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import logo from '../assets/logo.jpg';
+import { Link } from 'react-scroll';
 
 
 const Navbar = () => {
   return (
-   <nav>
-    <img src= {logo} alt="logo" />
+   <nav className='h-20 w-screen my-0 mx-auto py-4 px-8 flex items-center justify-between sticky top-0 z-10 max-w-6xl'>
+    <img className='size-20' src= {logo} alt="logo" />
     <div className="destopMenu">
-
+    <Link>Home</Link>
+    <Link>About</Link>
+    <Link>Portfolio</Link>
+    <Link>Client</Link>
+    
     </div>
     <button className='destopMenuBtn'>
       <img src="" alt="" /> Contact Me</button>
