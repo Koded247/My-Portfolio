2afa9d7bfd5fcf3232e2d5d961855d2f90e30056
abidoye korede <korexcoded@gmail.com>
2024-10-20 23:37:17 +0100

    updated files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,15 +6,15 @@ import { Link } from 'react-scroll';
 const Navbar = () => {
   return (
    <nav className='h-20 w-screen my-0 mx-auto py-4 px-8 flex items-center justify-between sticky top-0 z-10 max-w-6xl'>
-    <img className='size-20' src= {logo} alt="logo" />
-    <div className="destopMenu">
-    <Link>Home</Link>
+    <img className='size-20 object-cover ' src= {logo} alt="logo" />
+    <div className=" cursor-pointer flex gap-4">
+    <Link className=''>Home</Link>
     <Link>About</Link>
     <Link>Portfolio</Link>
-    <Link>Client</Link>
+    <Link>Clients</Link>
     
     </div>
-    <button className='destopMenuBtn'>
+    <button className='destopMenuBtn bg-white text-black p-2 '>
       <img src="" alt="" /> Contact Me</button>
    </nav>
 
