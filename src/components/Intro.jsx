902a26b09fa3c83f0f60cbd9c52dde6e8961c0db
abidoye korede <korexcoded@gmail.com>
2024-10-21 00:56:00 +0100

    updated sections

---
+++ src/components/Intro.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-scroll';
+
+const Intro = () => {
+  return (
+    <section>
+      <div>
+        <span>Hello,</span>
+        <span>I'm</span><span>Akorede</span><br /> <span>Website Designer</span> <br /> 
+        <p>i am a skilled web designer with experience in creating visual</p>
+        <Link> <button>Hire me</button></Link>
+        
+      </div>
+
+    </section>
+  )
+}
+
+export default Intro
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from '../assets/logo.jpg';
 import { Link } from 'react-scroll';
+import { MessageSquareQuote } from 'lucide-react';
 
 
 const Navbar = () => {
@@ -8,14 +9,14 @@ const Navbar = () => {
    <nav className='h-20 w-screen my-0 mx-auto py-4 px-8 flex items-center justify-between sticky top-0 z-10 max-w-6xl'>
     <img className='size-20 object-cover ' src= {logo} alt="logo" />
     <div className=" cursor-pointer flex gap-4">
-    <Link className=''>Home</Link>
-    <Link>About</Link>
-    <Link>Portfolio</Link>
-    <Link>Clients</Link>
+    <Link className=' hover:text-yellow-300 hover:pb-2 hover:border-b-4 '>Home</Link>
+    <Link className=' hover:text-yellow-300 hover:pb-2 hover:border-b-4 '>About</Link>
+    <Link className=' hover:text-yellow-300 hover:pb-2 hover:border-b-4 '>Portfolio</Link>
+    <Link className=' hover:text-yellow-300 hover:pb-2 hover:border-b-4 '>Clients</Link>
     
     </div>
-    <button className='destopMenuBtn bg-white text-black p-2 '>
-      <img src="" alt="" /> Contact Me</button>
+    <button className='destopMenuBtn bg-white text-sm text-black p-3 border-none rounded-3xl flex justify-center items-center gap-2 '>
+    <MessageSquareQuote />  Contact Me</button>
    </nav>
 
   )
