1010a48fe9712cbf55d428485d225fa74395278c
abidoye korede <korexcoded@gmail.com>
2024-12-07 14:22:11 -0800

    updated file

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import bg from '../assets/profile.jpeg';
+import bg from '../assets/portfoto.jpeg';
 import { Link } from 'react-scroll';
 
 const Intro = () => {
   return (
     <section id='Home' className='w-screen max-w-6xl mx-auto h-screen flex flex-col md:flex-row items-center justify-between p-4 md:p-8'>
       {/* Left Side - Text */}
-      <div className='flex flex-col justify-center gap-4 w-full md:w-1/2 text-center md:text-left'>
+      <div className='flex flex-col justify-center gap-4 w-full md:w-1/2 text-center md:text-left shadow-2xl p-4'>
         <span className='text-3xl md:text-5xl font-semibold'>Hello,</span>
         <span className='text-3xl md:text-5xl'>I'm</span><span className='text-3xl md:text-5xl font-bold text-yellow-600'>Akorede</span>
         <span className='text-3xl md:text-5xl'>Website Designer</span>
@@ -21,8 +21,8 @@ const Intro = () => {
       </div>
 
       {/* Right Side - Image */}
-      <div className='w-full md:w-1/2 flex justify-center md:justify-end mt-8 md:mt-0'>
-        <img src={bg} alt="profile" className='h-auto w-2/3 md:w-1/2 object-cover rounded-full' />
+      <div className='w-full md:w-1/2 flex justify-center md:justify-end mt-8 md:mt-0 '>
+        <img src={bg} alt="profile" className='h-auto w-2/3 md:w-3/5 object-cover rounded-full ' />
       </div>
     </section>
   );
