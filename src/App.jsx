e0fce85e4e652113277587e07883f5f7b245b5e7
abidoye korede <korexcoded@gmail.com>
2024-12-07 15:00:04 -0800

    updated file

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,18 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Intro from './components/Intro';
-import Works from './components/Works';
-import Skill from './components/skill';
-import Contact from './components/Contact';
-import Footer from './components/Footer'
-
-
-
-
-
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import Intro from "./components/Intro";
+import Works from "./components/Works";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className=' '>
+    <div className=" ">
       <Navbar />
       <Intro />
       <Works />
-      <Skill />
       <Contact />
       <Footer />
-      
     </div>
   );
 }
